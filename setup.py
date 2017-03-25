e5dd8330aab1e651e399ea0687699a43f3cd8136
ace <azatibrakov@gmail.com>
2017-03-25 14:00:47 +0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 packages = find_packages()
 setup(name='beylerbey',
-      version='0.1.0',
+      version='0.1.1',
       packages=packages,
       install_requires=[
           'SQLAlchemy>=1.0.12',
