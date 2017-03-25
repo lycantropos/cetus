2c9794e2f105c6b64716cfe1b477b49419be51de
ace <azatibrakov@gmail.com>
2017-03-25 12:13:28 +0700

    Imports restructurized

--- beylerbey/queries/__init__.py
+++ beylerbey/queries/__init__.py
@@ -2,4 +2,5 @@ from .saving import generate_insert_query
 from .deletion import generate_delete_query
 from .reading import (generate_select_query,
                       generate_group_wise_query)
-from .utils import ALL_COLUMNS_ALIAS
+from .utils import (ALL_COLUMNS_ALIAS,
+                    ORDERS_ALIASES)
